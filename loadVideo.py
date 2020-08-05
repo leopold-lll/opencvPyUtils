2aff5446729c86e50e8517acb566064554be72bd
leopold-lll <stefano.leonardi-2@studenti.unitn.it>
2020-08-05 15:18:54 +0200

    Add simulate realtime application feature to LoadVideo.

--- loadVideo.py
+++ loadVideo.py
@@ -1,19 +1,22 @@
 # import the necessary packages
 from imutils.video import VideoStream, FileVideoStream, FPS
 #imutils: https://github.com/jrosebr1/imutils/tree/master/imutils/video
+import datetime
 import time
 import os
 
 class LoadVideo:
 	# The class process a video file or the webcam to return a frame when requested.
 
-	def __init__(self, source: str="0", warmUp: float=1.0):
+	def __init__(self, source: str="0", warmUp: float=1.0, simulateRealtime: int=0):
 		""" 
 		Initialization function of the LoadVideo class.
   
 		Parameters: 
 		source (str):		The path to the video to be load, or the number of the webcam. None is considered as webcam 0.
 		warmUp (float):		The number of seconds for warm up the webcam.
+		simulateRealtime(int):	If the flag is 0 the frame of the video are all used, if it is greater (fps of the input video) some frames are discarded in order to simultate a real-time processing. This flag influence only file video processing. (i.e. if 0.5 seconds pass from a request to the next one so some frames can be discarded because are already "passed" and "no more available")
+		  NB: this method only speedup the processing by burn some frames, if the processing is faster than the original fps of the video nothing is done.
 		"""
 		if source=="-1" or None:
 			source="0"
@@ -21,17 +24,25 @@ class LoadVideo:
 		self.source = source
 		self._fps = None
 		
-		if source.isdigit(): #loading form webcam stream
+		if source.isdigit(): #loading stream form webcam
 			self.realtime = True
 			self.stream = VideoStream(src=int(source)).start()
 			time.sleep(warmUp)	#warm up the camera
 		
-		else:				#loading form file stream
+		else:				#loading stream form file
 			self.realtime = False
 			if os.path.exists(source):
 				self.stream = FileVideoStream(source).start()
 			else:
 				print("Warning missing source file:", source)
+
+		#make simulateRealtime 0 even according to the type of source video
+		self.simulateRealtime = 0 if self.realtime or simulateRealtime==0 else simulateRealtime
+		if self.simulateRealtime!=0:
+			#set some parameters for the realtime simulation
+			self.millisecPerFrame = 1000/self.simulateRealtime	#how many millisec each frame took
+			self.totalframesDiscarded = 0						#total number of frames discarded and not processed
+			self.startTime = None								#time of start processing time
 
 	def read(self) -> "numpy.ndarray":
 		""" Return a new frame of the stream. """
@@ -41,9 +52,34 @@ class LoadVideo:
 		else:
 			# update the FPS counter
 			self._fps.update()
+
+		#get the new frame
+		if self.startTime is None:
+			self.startTime = datetime.datetime.now()
+
+		#burn old frame to simultate the realtime processing on video file
+		if self.simulateRealtime!=0:
+
+			now = datetime.datetime.now()						#get actual time
+			elapsed = (now-self.startTime).total_seconds()*1000	#millisecond passed from the first frame captured
+
+			oldDiscard = self.totalframesDiscarded							#total number of discarded frames
+			self.totalframesDiscarded = int(elapsed//self.millisecPerFrame)	#total number of frames to discard (from begin)
+
+			framesToDiscard = self.totalframesDiscarded - oldDiscard - 1	#how many frames left to burn
+			for _ in range(framesToDiscard):
+
+				#update the FPS counter (it seems like realtime)
+				#self._fps.update()			#delete this row to measure the real fps...
+
+				discard = self.stream.read()#burn already gone frames
+				if discard is None:
+					return None				#corner case: reach the end of the file video
+				
+		# get the frame that will be retrieved
 		return self.stream.read()
 
-	def fpsStart(self) -> None:
+	def _fpsStart(self) -> None:
 		""" Start the FramePerSecond calculator. """
 		self._fps = FPS().start()
 
@@ -51,10 +87,10 @@ class LoadVideo:
 		""" Compute the elapsed processing time. """
 		return self._fps.elapsed()
 
-	def fps(self, update: bool=True) -> float:
+	def fps(self, update: bool=False) -> float:
 		""" Compute the fps rate of the reading processing time. 
 		Parameters: 
-		update (bool):	If update the count of loops or simply, retrieve the fps rate.
+		update (bool):	If update the count of loops or simply, retrieve the fps rate, of the frames taken from the stream.
 		"""
 		if self._fps is not None:
 			if update:
